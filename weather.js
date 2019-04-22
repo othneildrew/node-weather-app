43048a8957b9909c1faad39fcaaff8dfb30b88c4
Othneil Drew <codeguydrew@gmail.com>
2019-04-22 10:55:21 -0500

    Refactor: Function for showing errors

--- weather.js
+++ weather.js
@@ -3,6 +3,11 @@ const api = require('./api.json');
 const APIKEY = api.key;
 
 
+function showError (error) {
+  console.error(error.white.bgRed);
+}
+
+
 function current (location, queryType) {
   const units = 'imperial';
   const query = queryType === 'zip' ? 'zip' : 'q';
@@ -25,20 +30,20 @@ function current (location, queryType) {
 
         if (json.cod === 200) {
           // Return weather if response is success (200)
-          let string = `\t${json.name} has ${json.weather[0].description}, temp of ${Math.round(json.main.temp)}F, and humidity of ${json.main.humidity}%`;
+          let string = `${json.name} has ${json.weather[0].description}, temp of ${Math.round(json.main.temp)}F, and humidity of ${json.main.humidity}%`;
 
           console.log(string.yellow);
 
         } else {
-          console.error(`\t${json.message} for ${location}`.white.bgMagenta);
+          showError(`${json.message} for ${location}`);
         }
       });
     }).on('error', error => {
-      console.error(error.message.red);
+      showError(error.message);
     });
 
   } catch (error) {
-    console.error(error.message.red);
+    showError(error.message);
   }
 }
 
